eeb16f7dcf9bde2424a3090fabdb1f82b209ebdd
mackliu <macklun@gmail.com>
2023-08-04 13:40:50 +0800

    建立訂單資料表並預建訂單資料

---
+++ tt.php
@@ -0,0 +1,11 @@
+<?php
+
+echo serialize([0]);
+echo serialize([2,3]);
+echo serialize([4,5,6]);
+echo serialize([10,11,12,13]);
+echo serialize([1]);
+echo serialize([11,12]);
+echo serialize([1,2,3,4]);
+echo serialize([1,2,13,14]);
+
