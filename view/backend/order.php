21581fe6382a040a9f9be431c078d0ae7311290c
mackliu <macklun@gmail.com>
2023-08-04 14:41:20 +0800

    建立訂單清單列表畫面

--- view/backend/order.php
+++ view/backend/order.php
@@ -15,3 +15,66 @@ foreach($movies as $movie){
 <button onclick="qDel()">刪除</button>
 
 </div>
+
+<style>
+    .header,
+    .order{
+        display:flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .header div{
+        width:calc(100% / 7);
+        background-color: #aaa;
+        text-align: center;
+        margin:0 1px;
+    }
+    .order div{
+        width:calc(100% / 7);
+        text-align: center;
+        margin:0 1px;
+    }
+    .order-list{
+        overflow:auto;
+        height:400px;
+    }
+
+</style>
+<div class="header">
+    <div>訂單編號</div>
+    <div>電影名稱</div>
+    <div>日期</div>
+    <div>場次時間</div>
+    <div>訂購數量</div>
+    <div>訂購位置</div>
+    <div>操作</div>
+</div>
+<div class="order-list">
+    <?php 
+    foreach($rows as $row){
+
+    ?>
+    <div class="order">
+        <div><?=$row['no'];?></div>
+        <div><?=$row['movie'];?></div>
+        <div><?=$row['date'];?></div>
+        <div><?=$row['session'];?></div>
+        <div><?=$row['qt'];?></div>
+        <div><?php
+            $seats=unserialize($row['seats']);
+            foreach($seats as $seat){
+
+                echo (floor($seat/5)+1)."排".(($seat%5)+1)."號";
+                echo "<br>";
+            }
+        ?></div>
+        <div>
+            <button class="del" data-id="<?=$row['id'];?>">刪除</button>
+        </div>
+    </div>
+    <hr>
+    <?php 
+        }
+
+    ?>
+</div>
