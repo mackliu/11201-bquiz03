754858b7269168709b628d8cff9ee6dc9331a6b9
mackliu <macklun@gmail.com>
2023-08-07 09:04:15 +0800

    建立院線片清單css

--- view/front/main.php
+++ view/front/main.php
@@ -10,7 +10,35 @@
     </div>
 </div>
 
-
+<style>
+.movies *{
+    box-sizing: border-box;
+}
+.movies{
+    display:flex;
+    flex-wrap:wrap;
+    justify-content: space-evenly;
+}
+.movie{
+    width:48%;
+    border:1px solid white;
+    padding:5px;
+    border-radius: 5px;
+    margin:2px 0;
+}
+.row{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+.poster{
+    width:30%;
+    height:90px;
+}
+.info div:nth-child(1){
+    font-size:18px;
+}
+</style>
 <div class="half">
     <h1>院線片清單</h1>
     <div class="rb tab" style="width:100%;">
