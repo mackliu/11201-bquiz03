5ef95c9a13f44c524742de6cfd98408a058c2ed1
mackliu <macklun@gmail.com>
2023-08-07 08:42:14 +0800

    建立院線片清單版型架構

--- view/front/main.php
+++ view/front/main.php
@@ -9,14 +9,37 @@
         </div>
     </div>
 </div>
+
+
 <div class="half">
     <h1>院線片清單</h1>
-    <div class="rb tab" style="width:95%;">
-        <table>
-            <tbody>
-                <tr> </tr>
-            </tbody>
-        </table>
-        <div class="ct"> </div>
+    <div class="rb tab" style="width:100%;">
+        <div class='movies'>
+            <div class="movie">
+                <div class="row">
+                    <div class="poster">
+                        <a href="?do=intro&id=">
+                            <img src="./upload/" style="width:100%;height:100%">
+                        </a>
+                    </div>
+                    <div class="info">
+                        <div></div>
+                        <div>分級:
+                            <img src="./icon/03C0 .png" style="width:20px;height:20px">
+
+                        </div>
+                        <div>
+                            上映日期:
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <button>劇情簡介</button>
+                    <button>線上訂票</button>
+                </div>
+            </div>
+
+        </div>
+        <div class="ct"> 123</div>
     </div>
 </div>
