8e8fffb9ace55d472ffdc9d95750221de1b43f0e
mackliu <macklun@gmail.com>
2023-08-11 09:27:21 +0800

    建立座位csss及帶入電影訂單資訊

--- api/get_booking.php
+++ api/get_booking.php
@@ -19,10 +19,34 @@ include_once "../base.php";
     min-height:100px;
     background:#ccc;
     margin:auto;
+    padding:5px 10%;
+}
+.seats{
+    width:315px;
+    height:340px;
+    margin:auto;
+    display:flex;
+    flex-wrap:wrap;
+    padding-top:18px;
+}
+.seat{
+    width:63px;
+    height:85px;
+    text-align: center;
 }
 </style>
-<div class="theater"></div>
-<div class="info ct">
-    <button onclick="$('#form,#booking').toggle()">上一步</button>  
-    <button>訂購</button>
+<div class="theater">
+    <div class="seats">
+        <div class="seat">X排X號</div>
+    </div>
+</div>
+<div class="info">
+
+    <div>您選擇的電影是:<?=$_GET['movie'];?></div>
+    <div>您選擇的時刻是:<?=$_GET['date'];?> <?=$_GET['session'];?></div>
+    <div>您已經<span id='tickets'></span>勾選張票，最多可以購買四張票</div>
+    <div class="ct">
+        <button onclick="$('#form,#booking').toggle()">上一步</button>  
+        <button>訂購</button>
+    </div>
 </div>
