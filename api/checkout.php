e98fba7da13743ed570a4714d1431dcab03be67d
mackliu <macklun@gmail.com>
2023-08-11 13:38:38 +0800

    修正訂單編號產生錯誤問題

--- api/checkout.php
+++ api/checkout.php
@@ -2,7 +2,9 @@
 include_once "../base.php";
 sort($_POST['seats']);
 $_POST['qt']=count($_POST['seats']);
-$_POST['no']=$Order->max("no")+1;
+
+$max_id=$Order->max('id')+1;
+$_POST['no']=date("Ymd") . sprintf("%04d",$max_id);
 $_POST['seats']=serialize($_POST['seats']);
 $Order->save($_POST);
 echo $_POST['no'];
